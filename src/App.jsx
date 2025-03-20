f0e446921e9d5e76c807047d2503d8fad3f4e1fc
MohdShariq1343 <shariqeyeweb@gmail.com>
2025-03-20 17:13:04 +0530

    first React

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css'
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import NotFound from './Pages/NotFound';
+
+function App() {
+
+  return (
+    <>
+      <Router>
+          <Routes>
+            <Route path="/"  element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                  <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
